671ff5c6c15f0faad8088bb2d2f2b63445ee53ed
minaz99 <hanymina43@gmail.com>
2024-03-13 00:30:19 +0100

    fix: removed old host

--- dbConfig.js
+++ dbConfig.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: "knowyourfriendsdb.cdcykqca66z2.eu-north-1.rds.amazonaws.com",
+  host: "",
   port: 5432,
   user: "mina",
   password: "minabolt99",
